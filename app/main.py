bc97175896de76422476a15eee9e39574cbc3198
aomms-hub <62050154@kmitl.ac.th>
2025-08-02 04:59:42 +0700

    [improve] add api disconnect db (for testing only)

--- app/main.py
+++ app/main.py
@@ -3,13 +3,15 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.service.redis_service import cache_status
 from app.client.db_client import database
-
+from app.models.common_response import CommonResponse, CommonStatus
+from app.constants.response import Responses
 from app.consumer.rabbit_consumer import RabbitClient
 from app.routers import consumer_route, dashboard_route
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s")
 
 rabbit_client = RabbitClient()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -26,10 +28,23 @@ async def lifespan(app: FastAPI):
         await database.disconnect()
         await rabbit_client.close()
 
+
 app = FastAPI(lifespan=lifespan)
 app.include_router(consumer_route.router)
 app.include_router(dashboard_route.router)
 
+
 @app.get("/")
 async def root():
-    return {"message": "🚀 Pay Alert Composite is alive!"}
+    return CommonResponse(
+        status=CommonStatus.from_enum(Responses.SUCCESS),
+        data="🚀 Pay Alert Composite is alive!"
+    )
+
+
+@app.post("/database/disconnect")
+async def disconnect_database():
+    await database.disconnect()
+    return CommonResponse(
+        status=CommonStatus.from_enum(Responses.SUCCESS)
+    )
--- app/routers/dashboard_route.py
+++ app/routers/dashboard_route.py
@@ -31,12 +31,14 @@ async def get_transaction_log_by_amount(amount: float):
         data=await transaction_log_service.get_transaction_log_by_amount(amount)
     )
 
+
 @router.post("/transaction_log", response_model=CommonResponse[int])
 async def insert_transaction_log(log: TransactionLogCreate):
     return CommonResponse(
         status=CommonStatus.from_enum(Responses.SUCCESS),
         data=await transaction_log_service.insert_transaction_log(log)
     )
+
 
 @router.get("/transaction_log_summarize", response_model=CommonResponse[float])
 async def get_sum_transaction_log():
