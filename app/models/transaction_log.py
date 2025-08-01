9b6c5dfd2dd9e3d0499c5df907227e00c9118af0
aomms-hub <62050154@kmitl.ac.th>
2025-08-02 03:46:40 +0700

    [improve] implement get and insert api

--- app/models/transaction_log.py
+++ app/models/transaction_log.py
@@ -2,10 +2,12 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
-class TransactionLog(BaseModel):
-    id: int
+class TransactionLogCreate(BaseModel):
     amount: float
     source: str
-    sound_url: Optional[str]
-    note: Optional[str]
+    sound_url: Optional[str] = None
+    note: Optional[str] = None
     timestamp: datetime
+
+class TransactionLog(TransactionLogCreate):
+    id: int
--- app/routers/dashboard_route.py
+++ app/routers/dashboard_route.py
@@ -1,10 +1,33 @@
 from fastapi import APIRouter
-from app.service.transaction_log_service import get_transaction_log_list
+from app.service import transaction_log_service
 from typing import List
-from app.models.transaction_log import TransactionLog
+from app.models.transaction_log import TransactionLog, TransactionLogCreate
 
 router = APIRouter(prefix="/dashboard")
 
+
+@router.get("/transaction_log_by_id", response_model=TransactionLog)
+async def get_transaction_log_by_id(transaction_id: int):
+    return await transaction_log_service.get_transaction_log_by_id(transaction_id)
+
+
 @router.get("/transaction_log_list", response_model=List[TransactionLog])
-async def list_transactions():
-    return await get_transaction_log_list()
+async def get_transaction_log_list():
+    return await transaction_log_service.get_transaction_log_list()
+
+
+@router.get("/transaction_log_by_amount", response_model=List[TransactionLog])
+async def get_transaction_log_by_amount(amount: float):
+    return await transaction_log_service.get_transaction_log_by_amount(amount)
+
+
+@router.post("/transaction_log", response_model=TransactionLog)
+async def insert_transaction_log(log: TransactionLogCreate):
+    id = await transaction_log_service.insert_transaction_log(log)
+    return id
+
+
+@router.get("/transaction_log_summarize")
+async def get_sum_transaction_log():
+    total = await transaction_log_service.get_total_transaction_amount()
+    return {"total_amount": total}
