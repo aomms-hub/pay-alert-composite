6bc0ca613f51d760660cde4e0307997e5552611f
aomms-hub <62050154@kmitl.ac.th>
2025-08-02 14:51:09 +0700

    [improve] add step insert db

--- app/service/notification_service.py
+++ app/service/notification_service.py
@@ -1,6 +1,16 @@
 from app.client.amount_tts_service_client import send_amount
+from app.service.transaction_log_service import insert_transaction_log
+from app.models.transaction_log import TransactionLogCreate
 
 
 async def notify(message: dict):
     tts_response = await send_amount(message['amount'])
+    transaction_log = TransactionLogCreate(
+        amount=message['amount'],
+        source=message['source'],
+        timestamp=message['timestamp'],
+        note=None,
+        sound_url=tts_response['audio_url']
+    )
+    await insert_transaction_log(transaction_log)
     print(tts_response)
